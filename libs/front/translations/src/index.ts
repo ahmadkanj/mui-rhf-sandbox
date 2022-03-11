0e02f56ac71b2bd001cbcf6aa6dd6d1b21a40668
jpb06 <jp.bois.06@outlook.fr>
2022-03-11 16:46:48 +0100

    adding namespaces

---
+++ libs/front/translations/src/index.ts
@@ -0,0 +1,4 @@
+export * from './types/namespace-key.type';
+
+export * from './keys/locales.keys';
+export * from './values/locales-values';
---
+++ libs/front/translations/src/values/locales-values.ts
@@ -0,0 +1,5 @@
+import enCommonLocale from './../../assets/locales/en/common.json';
+import enFormsLocale from './../../assets/locales/en/forms.json';
+
+export const commonValues = Object.values(enCommonLocale);
+export const formsValues = Object.values(enFormsLocale);
